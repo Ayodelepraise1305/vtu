01490124f674b87368d9593fee48efc41216f4e4
Praise <ayodelepraize1937@gmail.com>
2026-05-30 09:17:21 +0100

    fix port issue

--- backend/server.js
+++ backend/server.js
@@ -22,7 +22,7 @@ mongoose.connect(MONGO_URI)
   console.log("MongoDB connected successfully");
 
   // IMPORTANT: use Render port
-  const PORT = process.env.PORT || 5000;
+  const PORT = process.env.PORT || 3000;
 
   app.listen(PORT, () => {
     console.log("Server running on port " + PORT);
